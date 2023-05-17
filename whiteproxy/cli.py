67ed485646799ffbe4b33fda84e7d0e684ea67c8
Quantum Snowball <quantum.snowball@gmail.com>
2023-05-18 04:36:47 +0800

    feat: simple click interface

--- whiteproxy/cli.py
+++ whiteproxy/cli.py
@@ -1,2 +1,13 @@
-def whiteproxy() -> None:
-    print('whiteproxy hello world')
+import click
+
+
+@click.command()
+@click.option('-s', '--src', required=True, help='proxy source socket')
+@click.option('-d', '--dest', required=True, help='proxy destination socket')
+@click.option('-a', '--allow', required=True, multiple=True, help='whitelist ip addresses')
+def whiteproxy(src: str,
+               dest: str,
+               allow: list[str]) -> None:
+    print(src)
+    print(dest)
+    print(allow)
