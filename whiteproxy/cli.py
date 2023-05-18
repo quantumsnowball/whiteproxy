840c1e9ea9963431f944d2e9792ceef10937d6a0
Quantum Snowball <quantum.snowball@gmail.com>
2023-05-18 13:23:35 +0800

    step: target and proxy concurrently

--- whiteproxy/cli.py
+++ whiteproxy/cli.py
@@ -1,22 +1,56 @@
 import click
 
 import asyncio
+from asyncio import StreamReader, StreamWriter
 from whiteproxy.utils.console import print_warning
 
 
-async def async_exec(command: str, *args: str) -> None:
+async def start_target(command: str,
+                       *args: str) -> None:
+    # start subprocess
     proc = await asyncio.create_subprocess_exec(command, *args)
+    print_warning(f'{command} {" ".join(args)}')
     await proc.wait()
 
 
+async def start_proxy(host: str,
+                      target: str) -> None:
+    # src and dest socket
+    host_addr, host_port = host.split(':')
+    target_addr, target_port = target.split(':')
+    print_warning(f'Redirect: {host_addr}:{host_port} -> {target_addr}:{target_port}')
+
+    # server
+    async def serve(reader: StreamReader, writer: StreamWriter) -> None:
+        # confirm ip is in whitelist
+        # TODO
+        addr, port = writer.get_extra_info('peername')
+        print(f'Incoming: {addr}:{port}')
+
+        while True:
+            print('reading... ')
+            data = await reader.read(1024)
+            if not data:
+                break
+
+        print('ended.')
+        # await async_exec(command, *args)
+        # start proxy redirection
+        # TODO
+
+    server = await asyncio.start_server(serve, host_addr, host_port, limit=1)
+    async with server:
+        await server.serve_forever()
+
+
 @click.command()
-@click.option('-s', '--src', metavar='IP:PORT', required=True, help='proxy source socket')
-@click.option('-d', '--dest', metavar='IP:PORT', required=True, help='proxy destination socket')
+@click.option('-h', '--host', metavar='IP:PORT', required=True, help='proxy host socket')
+@click.option('-t', '--target', metavar='IP:PORT', required=True, help='proxy target socket')
 @click.option('-a', '--allow', required=True, multiple=True, help='whitelist ip addresses')
 @click.argument('command', nargs=1, type=str)
 @click.argument('args', nargs=-1, type=str)
-def whiteproxy(src: str,
-               dest: str,
+def whiteproxy(host: str,
+               target: str,
                allow: list[str],
                command: str,
                args: list[str]) -> None:
@@ -24,9 +58,9 @@ def whiteproxy(src: str,
     Run COMMAND and proxy serve the service
     (use `--` as prefix if COMMAND uses its own options)
     '''
-    print_warning(src)
-    print_warning(dest)
-    print_warning(', '.join(allow))
-
-    print_warning(f'{command} {" ".join(args)}')
-    asyncio.run(async_exec(command, *args))
+    async def tasks() -> None:
+        await asyncio.gather(
+            asyncio.create_task(start_target(command, *args)),
+            asyncio.create_task(start_proxy(host, target)),
+        )
+    asyncio.run(tasks())
